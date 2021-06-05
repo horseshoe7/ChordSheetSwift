c583f3b83f46ce080b4d09d4f19d451db07c8320
oconnorfreelance <oconnor.freelance@gmail.com>
2021-06-05 15:32:04 +0200

    Initial Commit

---
+++ ChordSheetSwift/ChordSheetSwift.h
@@ -0,0 +1,18 @@
+//
+//  ChordSheetSwift.h
+//  ChordSheetSwift
+//
+//  Created by Stephen O'Connor on 05.06.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChordSheetSwift.
+FOUNDATION_EXPORT double ChordSheetSwiftVersionNumber;
+
+//! Project version string for ChordSheetSwift.
+FOUNDATION_EXPORT const unsigned char ChordSheetSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChordSheetSwift/PublicHeader.h>
+
+
